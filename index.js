931986df54c06508747d406cc0667b24a1b43917
liutongxing <liutongxing@gmail.com>
2016-03-11 13:47:10 +1300

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ var express = require('express');
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+var _ = require("underscore");
 
 app.use(express.static('public'));
 
